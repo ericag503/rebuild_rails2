dd457e95c915d187c5d15ec8dec9da751d264e19
Erica Gordon <student@epicodus.com>
2014-09-10 15:46:42 -0700

    Add flash and edit index.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,8 +4,22 @@ class LessonsController < ApplicationController
     render('lessons/index.html.erb')
   end
 
+  def new
+    render('lessons/new.html.erb')
+  end
+
+  def create
+    @lesson = Lesson.new(params[:lesson])
+    if @lesson.save
+      flash[:notice] = "The lesson has been added!"
+      redirect_to("/lessons/")
+    else
+      render('lessons/new.html.erb')
+    end
+  end
+end
+
   def show
     @lesson = Lesson.find(params[:id])
     render('lessons/show.html.erb')
   end
-end
